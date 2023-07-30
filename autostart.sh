6be4ffce76efe2cc0283e4c3e65abedafcb0eea9
Igor Glajcher <kasetonix@gmail.com>
2023-07-30 17:32:36 +0200

    [30.07.23] Modified the autostart to draw the statusbar using the new rust implementation

--- autostart.sh
+++ autostart.sh
@@ -2,13 +2,7 @@
 
 # Setting variables for scripts
 scrDir="/home/kasetonix/.scripts"
-
-scrCapsL="$scrDir/capsl"
-scrBright="$scrDir/brightness"
-scrVolume="$scrDir/volume"
-scrBattery="$scrDir/battery"
-scrBatteryNotification="$scrDir/battery-notification"
-scrClock="$scrDir/clock"
+statusbar="$scrDir/statusbar"
 
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 xrandr --output eDP-1 --primary --mode 1920x1080 --refresh 60 --output HDMI-A-1-0 --mode 1920x1080 --refresh 75 --same-as eDP-1 & 
@@ -24,6 +18,6 @@ while [[ $(pidof dwm) != "" ]]; do
 done &
 
 while [[ $(pidof dwm) != "" ]]; do
-    xsetroot -name "$($scrCapsL)$($scrVolume)$($scrBright)$($scrBattery)$($scrClock)"
+    xsetroot -name "$($statusbar)"
     sleep 0.1
 done &
